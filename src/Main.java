1f20a29bf4e25c344e95da2393146d881c2534fc
NaurbievDjamaldin <okkslmalishka@gmail.com>
2026-05-19 10:22:49 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+/**
+ * @author Djamaldin N.
+ * @version 19.05.2026
+ */
 
+//Это класс "Main"
 public class Main {
+    //psvm - быстрый вызов public static void main
+    public static void main(String[] args) {
+        // этот метод является точкой входа в нашу программу "main"
+        //sout - быстрый вызов system out println
+        /*
+        Эта инструкция
+        выводит текст
+         */
+        System.out.println("Hello, My name is Djamaldin");
+    }
 }
