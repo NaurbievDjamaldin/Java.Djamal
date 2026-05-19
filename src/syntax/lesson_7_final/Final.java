af88b4e4686b94a2740b4b62be925f4eb600dc68
NaurbievDjamaldin <okkslmalishka@gmail.com>
2026-05-19 19:37:08 +0300

    final commit

---
+++ src/syntax/lesson_7_final/Final.java
@@ -0,0 +1,12 @@
+package syntax.lesson_7_final;
+
+public class Final {
+    public static void main(String[] args) {
+        final int firsInt = 4,  secondInt = 8;
+
+        System.out.println("firsInt= " + firsInt+", secondInt= "+secondInt);
+
+        final var nameOfBoss = "Djamal Naurbiev";
+        System.out.println(nameOfBoss);
+    }
+}
