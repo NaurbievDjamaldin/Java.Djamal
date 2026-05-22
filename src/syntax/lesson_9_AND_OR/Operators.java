de3f618a104d7109d37579b19699eddfe52f0040
NaurbievDjamaldin <okkslmalishka@gmail.com>
2026-05-22 16:05:38 +0300

    ANd_Or commit

---
+++ src/syntax/lesson_9_AND_OR/Operators.java
@@ -0,0 +1,21 @@
+package syntax.lesson_9_AND_OR;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        int a = 10, b = 3;
+
+        //операции сравнения
+        boolean isEqual = a == b;
+        boolean isNotEqual = a != b;
+        boolean isGreater = a > b;
+        boolean isGreaterOrEqual = a >= b;
+        boolean isLess = a < b;
+        boolean isLessOrEqual = a <= b;
+
+
+
+
+
+    }
+}
