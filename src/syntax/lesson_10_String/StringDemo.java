eaf871bfd9b15ad4a81ff21e660dd672780c4700
NaurbievDjamaldin <okkslmalishka@gmail.com>
2026-05-26 13:15:18 +0300

    String commit

---
+++ src/syntax/lesson_10_String/StringDemo.java
@@ -0,0 +1,9 @@
+package syntax.lesson_10_String;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String strName = "Djamal";
+        String  strLastName = new String("Naurbiev");
+        System.out.println(strName + " " + strLastName);
+    }
+}
