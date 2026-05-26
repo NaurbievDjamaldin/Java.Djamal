9fdb5cc2c82e1adeb94e49e6e3ac0f2f61a2c8b8
NaurbievDjamaldin <okkslmalishka@gmail.com>
2026-05-26 14:34:59 +0300

    kotlin commit

---
+++ src/syntax/lesson_11_kotlin_val_var/Main.kt
@@ -0,0 +1,16 @@
+package syntax.lesson_11_kotlin_val_var
+
+fun main() {
+
+    // java
+    // int nameOfVariable = 5; вот так обляются переменные в Java
+
+    //kotlin
+    var intNumber: Int = 5
+    var someString: String = "My name is Djamaldin!"
+
+    intNumber = 8
+    //val - не изменная переменная
+    val nameOfManager = "Djamaldin Naurbiev"
+
+}
